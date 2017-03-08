a6eb7241646a0302cce2bdcfca1e6e99358b7c3b
冯建涛 <gastonfeng@qq.com>
2017-03-08 08:29:38 +0800

    ..

--- builder/models/utils/jinja2/utils.py
+++ builder/models/utils/jinja2/utils.py
@@ -2,11 +2,27 @@ def groups_attribute(groups):
     return 'groups="{list}"'.format(list=','.join([i.real_xml_id for i in groups])) if len(groups) else ''
 
 
+def lst2unicode(lst):
+    st = u''
+    if isinstance(lst, list) or isinstance(lst, tuple):
+        st += '('
+        for l in lst:
+            if isinstance(l, unicode) or isinstance(l, str):
+                st += "u'%s'" % l
+                st += ','
+            elif isinstance(l, list) or isinstance(l, tuple):
+                st += lst2unicode(l)
+            else:
+                raise
+        st += '),'
+    return st
+
 def field_options(options):
     opts = []
     for op in options:
-        opts.append(('u' + op.value.encode("unicode-escape").decode("unicode-escape"), 'u' + op.name.encode("unicode-escape").decode("unicode-escape")))
-    return repr(opts)
+        opts.append((op.value, op.name))
+    opts = lst2unicode(opts)
+    return opts
 
 
 def field_attrs(field):
