1ad05b0b8c3ee92125cf3a6c538016b80db6ce19
gastonfeng <gastonfeng@gmail.com>
2017-10-15 17:10:57 +0800

    统一构建脚本

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-yum install rsync python-setuptools -y
-python -V
-rm *.zip -rf
-easy_install odoorpc
-chmod 600 rsyncd.password
-rm -rf /var/log/odoo/odoo-server.log
-python mywork/tools/build_odoo.py
