0aa94a64540459ae6a8d5e42a8ab213b9bfab9c2
gastonfeng <gastonfeng@gmail.com>
2018-08-10 16:55:56 +0800

    bug fix: import

--- prj.py
+++ prj.py
@@ -2,7 +2,6 @@
 from mywork.project.odooModule import odooModule
 from mywork.project.project import project
 from mywork.stocks.gits import gits
-from mywork.tools._jenkins import jenkins_task, jenkins_server
 from mywork.tools.build_odoo import build_odoo
 from mywork.workSpace.store import store
 
@@ -10,7 +9,6 @@ de = odooModule('developer')
 sw = odooModule('software')
 
 om = build_odoo('builder', "1.0", git=gits('builder', url='ssh://kaikong@192.168.31.10/home/git/odoo_builder.git'),
-                jenkins_task=jenkins_task('odoo_builder', runWith=jenkins_server('ubuntu16')),
                 store=store('kaikong.com.cn'), deploy=['www.kaikong.cn', ],
                 depends=[de, sw, ])
 prj = project('builder', products=[om, ], )
