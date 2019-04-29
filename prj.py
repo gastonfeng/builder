fa747370375fdf95c87feebf865fb0b3bde52c5d
gastonfeng <gastonfeng@gmail.com>
2019-04-29 13:27:24 +0800

    api caller update

--- prj.py
+++ prj.py
@@ -8,9 +8,7 @@ from mywork.workSpace.store import store
 de = odooModule('developer')
 sw = odooModule('software')
 
-om = build_odoo('builder', "1.0", git=gits('builder', url='ssh://kaikong@192.168.31.10/home/git/odoo_builder.git'),
+om = build_odoo('builder', "1.0", git=gits('builder', url='ssh://kaikong@192.168.31.10/home/git/odoo_builder.git'),newTests=['test11'],oldTests=['odoo11'],
                 store=store('kaikong.com.cn'), deploy=['www.kaikong.cn', ],
                 depends=[de, sw, ])
-prj = project('builder', products=[om, ], )
-prj.output('builder')
-prj.build()
+om.build()
